97841d62a7c5c9895fcbe6a6210e00f4b3346316
Shim008 <faisal.shimanto.grn13@gmail.com>
2017-10-11 14:13:37 +0600

    Create Substraction.java

---
+++ Substraction.java
@@ -0,0 +1,18 @@
+
+package calculator;
+
+public class Substraction {
+
+    double sub;
+
+    public Substraction(double a,double b)
+    {
+        sub = a - b;
+    }
+
+    public double getSub()
+    {
+        return sub;
+    }
+
+}
