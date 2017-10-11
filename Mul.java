e52ed4591d91f31d6f4b5d65d5cf3b7fd7c09b93
Shim008 <faisal.shimanto.grn13@gmail.com>
2017-10-11 14:14:25 +0600

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Mul {
+
+    private double mulS;
+
+    public Mul(double a , double b){
+        mulS = a * b;
+
+    }
+     public double getMul(){
+         return mulS;
+     }
+
+}
