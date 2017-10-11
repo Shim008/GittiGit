b056cbeb7dfe0ef5739a130dd8ad486b3f4ee291
Shim008 <faisal.shimanto.grn13@gmail.com>
2017-10-11 14:20:19 +0600

    Add files via upload

---
+++ Calculator/src/calculator/Div.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Div {
+
+    private double divS;
+
+    public Div(double a , double b){
+        divS = a / b;
+
+    }
+     public double getDiv(){
+         return divS;
+     }
+
+}
